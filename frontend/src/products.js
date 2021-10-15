82b5eee7ced6835df21c58d3d508b7ba0e59f3fc
Arne Tripolone <arnetripolone@gmail.com>
2021-10-15 12:11:14 +1100

    product images and products js added

---
+++ frontend/src/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    _id: '1',
+    name: 'OP Violin - Book 1',
+    image: '/images/opviolinbook1.jpg',
+    description:
+      'First of four books in the OP Violin series. Learn about posture, open strings, basic note reading and fun violin games to start your violin journey',
+    brand: 'OP Violin',
+    category: 'E-books',
+    price: 9.99,
+    countInStock: 10,
+    rating: 5.0,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'OP Violin - Book 2',
+    image: '/images/opviolinbook2.jpg',
+    description:
+      'Second of four books in the OP Violin series. Learn all 4 fingers on all 4 strings, along with fun exerises to train finger strength and agility',
+    brand: 'OP Violin',
+    category: 'E-books',
+    price: 11.99,
+    countInStock: 10,
+    rating: 5.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'OP Violin - Book 3',
+    image: '/images/opviolinbook3.jpg',
+    description:
+      'Third of four books in the OP Violin series. Introduction of key signatures and sight reading',
+    brand: 'OP Violin',
+    category: 'E-books',
+    price: 13.99,
+    countInStock: 10,
+    rating: 5.0,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'OP Violin - Book 4',
+    image: '/images/opviolinbook4.jpg',
+    description:
+      'Fourth of four books in the OP Violin series. Choose your own adventure/repertoire and get prepared for your first performance!',
+    brand: 'OP Violin',
+    category: 'E-books',
+    price: 20.00,
+    countInStock: 10,
+    rating: 5.0,
+    numReviews: 12,
+  }
+]
+
+export default products
