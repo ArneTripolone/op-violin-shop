ba2a9ba5546fe142a4b2c27d8305e6126dfaac91
Arne Tripolone <arnetripolone@gmail.com>
2021-10-31 20:26:12 +1100

    Improving cart actions

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,10 +2,16 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-
 import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id
+    
+    const qty = location.search ? location.search.split('=')[1] :
+
+    console.log
+    
     return (
         <div>
             Cart
