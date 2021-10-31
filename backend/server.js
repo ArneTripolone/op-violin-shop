00963b64d4e076dbfcab9274bcd4b28c42329f0b
Arne Tripolone <arnetripolone@gmail.com>
2021-10-31 23:06:35 +1100

    Bug fix for userRoutes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
